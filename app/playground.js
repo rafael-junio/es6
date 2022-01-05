f1a3d0da3d8f6c60580d3f1f5e02631797820cec
rafael-junio <rafael-954@hotmail.com>
2022-01-05 08:59:58 -0400

    typescript types

---
+++ app/playground.js
@@ -0,0 +1,9 @@
+var myString = 'abc';
+var myBoolean = true;
+var myNumber = 10.5;
+var myArray = ['A', 'B', 'C'];
+var myTuple = ['A', 10];
+console.log(myString);
+console.log(myBoolean);
+console.log(myNumber);
+console.log(myArray);
---
+++ app/playground.ts
@@ -0,0 +1,10 @@
+const myString: string = 'abc';
+const myBoolean: boolean = true;
+const myNumber: number = 10.5;
+const myArray: string[] = ['A', 'B', 'C'];
+const myTuple: [string, number] = ['A', 10];
+
+console.log(myString);
+console.log(myBoolean);
+console.log(myNumber);
+console.log(myArray);
